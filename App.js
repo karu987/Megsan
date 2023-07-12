8f0d27c7272b4704587ca0215850c49598668447
kartheekreddy2 <d.kartheekreddy@megsandiagnostics.com>
2023-07-12 12:06:39 +0530

    all commits

---
+++ App.js
@@ -0,0 +1,44 @@
+alert("Allow Notifications");
+let name = 'Neymar'; //string
+let age = 30; //number
+let isworldcup = false //boolean
+let person = {
+    name: 'Neymar',
+    age: 30
+
+}
+let selectedteams = ['Santos', 'Barcelona', 'PSG']
+console.log(name); //its log in the console
+console.log(age);
+console.log(selectedteams);
+function greet(name, lastName) {
+    console.log('hello ' +  name+' ' + lastName);
+}
+greet('Neymar', 'Jr')
+
+//performing a task
+
+
+
+//calculating a value
+function square(number) {
+    return number * number
+}
+console.log(square(266));
+
+function percentage(number1,number2) {
+    return number1 / number2 * 100
+}
+console.log(percentage(95,500));
+
+function cubes(number) {
+    return number * number * number
+}
+console.log(cubes(3));
+
+function avg(a,b,c,d,e,f,g) {
+    return (a+b+c+d+e+f+g)/7
+}
+console.log(avg(100,50,20,30,40,60,90))
+
+
